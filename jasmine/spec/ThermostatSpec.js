54030ec14f99c79873cb302e35b740265820b9e7
Bernard Malhame <bernard.malhame@gmail.com>
2017-03-15 15:21:21 +0000

    added min temp and power saving modes

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -5,6 +5,23 @@ describe("Thermostat", function(){
     expect(thermostat.currentTemperature).toEqual(20);
   });
 
+  it('has a minimum temperature of 10', function(){
+    expect(thermostat.minimumTemperature).toEqual(10);
+  });
+
+  it('is on power saving mode as default setting', function(){
+    expect(thermostat.powerSavingMode).toBe(true);
+  });
+
+  it('has a max temperature of 25 when power saving on', function(){
+    expect(thermostat.maximumTemperature).toEqual(25);
+  });
+
+  it('has a max temperature of 32 when power saving off', function(){
+    thermostat.powerSavingOff();
+    expect(thermostat.maximumTemperature).toEqual(32);
+  });
+
   describe("Temperature change", function() {
     beforeEach(function() {
       thermostat.currentTemperature = 20;
@@ -23,5 +40,7 @@ describe("Thermostat", function(){
         expect(thermostat.currentTemperature).toEqual(19);
       });
     });
+
+
   });
 });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,6 +1,11 @@
 function Thermostat(){
+  this.minimumTemperature = MINIMUM_TEMPERATURE;
   this.currentTemperature = 20;
+  this.powerSavingMode = true;
+  this.maximumTemperature = this._determineMaxTemperature();
 };
+
+const MINIMUM_TEMPERATURE = 10;
 
 Thermostat.prototype.up = function() {
   this.currentTemperature ++;
@@ -8,4 +13,18 @@ Thermostat.prototype.up = function() {
 
 Thermostat.prototype.down = function() {
   this.currentTemperature --;
+};
+
+Thermostat.prototype.powerSavingOff = function() {
+  this.powerSavingMode = false;
+  this._determineMaxTemperature();
+};
+
+Thermostat.prototype._determineMaxTemperature = function() {
+  if (this.powerSavingMode === true){
+    return this.maximumTemperature = 25;
+  } else {
+    return this.maximumTemperature = 32;
+  };
+
 };
