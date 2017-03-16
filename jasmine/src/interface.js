4b65f135a860c54caef464adf9c4b7188dec38f7
Katie Koschland <katiekoschland@hotmail.com>
2017-03-16 16:37:54 +0000

    experimenting with the interface

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
+    updateTemperature()
 
   $('h1').text("THERMOSTAT");
-  $("#currentTemp").text(thermostat.currentTemperature);
+  // $("#currentTemp").text(thermostat.currentTemperature);
   $("#powerSavingMode").text(thermostat.powerSavingMode);
   $("#energyUsage").text(thermostat.checkEnergyUsage());
 
   $("#up").on("click", function() {
     thermostat.up();
+    updateTemperature()
     $("#currentTemp").text(thermostat.currentTemperature);
     $("#energyUsage").text(thermostat.checkEnergyUsage());
   })
 
   $("#down").on("click", function() {
     thermostat.down();
+    updateTemperature()
     $("#currentTemp").text(thermostat.currentTemperature);
     $("#energyUsage").text(thermostat.checkEnergyUsage());
   })
 
   $("#reset").on("click", function() {
     thermostat.reset();
+    updateTemperature()
     $("#currentTemp").text(thermostat.currentTemperature);
   })
 
@@ -32,4 +36,9 @@ $(document).ready(function(){
     thermostat.powerSavingOff();
     $("#powerSavingMode").text(thermostat.powerSavingMode);
   })
+
+  function updateTemperature() {
+    $("#currentTemp").text(thermostat.currentTemperature);
+    $("#currentTemp").attr('class', thermostat.checkEnergyUsage());
+  }
 });
