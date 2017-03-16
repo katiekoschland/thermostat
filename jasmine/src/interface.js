576608df41a09b40e4461423508b272466b7ccfd
Katie Koschland <katiekoschland@hotmail.com>
2017-03-16 13:00:18 +0000

    Power saving and up buttons functioning with jQuery

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -3,10 +3,30 @@ $(document).ready(function(){
 
   $('h1').text("THERMOSTAT");
   $("#currentTemp").text(thermostat.currentTemperature);
+  $("#powerSavingMode").text(thermostat.powerSavingMode);
 
   $("#up").on("click", function() {
     thermostat.up();
     $("#currentTemp").text(thermostat.currentTemperature);
   })
 
+  $("#down").on("click", function() {
+    thermostat.down();
+    $("#currentTemp").text(thermostat.currentTemperature);
+  })
+
+  $("#reset").on("click", function() {
+    thermostat.reset();
+    $("#currentTemp").text(thermostat.currentTemperature);
+  })
+
+  $("#psON").on("click", function() {
+    thermostat.powerSavingOn();
+    $("#powerSavingMode").text(thermostat.powerSavingMode);
+  })
+
+  $("#psOFF").on("click", function() {
+    thermostat.powerSavingOff();
+    $("#powerSavingMode").text(thermostat.powerSavingMode);
+  })
 });
